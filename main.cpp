bd49cc496e733b281a1af7faa929346d3387eb5a
faeton-off <faetonoff@gmail.com>
2021-05-29 17:53:34 +0200

    dfssadfsdfsafafsfsdfs

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     TetrixWindow window;//jjdjd
-    window.show(); //ыfsdfssdsvavs
+    window.show(); //próba
     return app.exec();
 }
